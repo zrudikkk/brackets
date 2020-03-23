faa0dc73098ccb4cb1c84fb654a04805cbb75575
Alexey-Koren <johnsmitlpf@gmail.com>
2020-03-23 20:14:22 +0300

    fix: test.js error messages

--- test.js
+++ test.js
@@ -34,7 +34,7 @@ it('should check if brackets sequence is correct 6', () => {
   assert.equal(check('[]()', config2), true);
 });
 
-it('should check if brackets sequence is correct 7', () => {
+it('should check if brackets sequence is not correct 7', () => {
   assert.equal(check('[]()(', config2), false);
 });
 
@@ -58,11 +58,11 @@ it('should check if brackets sequence is correct 12', () => {
   assert.equal(check('111115611111111222288888822225577877778775555666677777777776622222', config6), true);
 });
 
-it('should check if brackets sequence is correct 13', () => {
+it('should check if brackets sequence is not correct 13', () => {
   assert.equal(check('5555512575557777777555566667888888667661133833448441111222233333444442266666', config6), false);
 });
 
-it('should check if brackets sequence is correct 14', () => {
+it('should check if brackets sequence is not correct 14', () => {
   assert.equal(check('8888877878887777777888888887777777887887788788887887777777788888888887788888', config6), false);
 });
 
