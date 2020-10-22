9410b93e363310211a48b85eef819c46680cdb63
Zrudikkk <zaburianov@gmail.com>
2020-10-22 19:09:26 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    let openBrackets = bracketsConfig.map(brk => brk[0]);
+    let closeBrackets = bracketsConfig.map(brk => brk[1]);
+    for (let i = 0; i < str.length; i++){
+        let n = openBrackets.indexOf(str[i]);
+        if (n > -1) {
+            arr.push(closeBrackets[n]);
+        }
+        if (closeBrackets.includes(str[i]) && str[i] !== arr.pop()) {
+            return false;
+        }
+    }
+    return arr.length === 0;
 }
